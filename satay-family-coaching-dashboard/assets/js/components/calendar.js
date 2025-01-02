2eefb5dcdfa2612c55b990060313acfcc2cb9b5d
Carson Cruz <carson@comcreate.org>
2025-01-02 13:59:57 -0500

    Delete satay-family-coaching-dashboard directory

--- satay-family-coaching-dashboard/assets/js/components/calendar.js
+++
@@ -1,9 +0,0 @@
-// calendar.js
-async function loadContentCalendarData() {
-  const response = await fetch(
-    "/satay-family-coaching-dashboard/data/content-calendar.json"
-  );
-  const data = await response.json();
-  console.log("Content Calendar Data:", data);
-}
-document.addEventListener("DOMContentLoaded", loadContentCalendarData);
--- satay-family-coaching-dashboard/assets/js/components/tables.js
+++
@@ -1,9 +0,0 @@
-// tables.js
-async function loadKeywordData() {
-  const response = await fetch(
-    "/satay-family-coaching-dashboard/data/keywords.json"
-  );
-  const data = await response.json();
-  console.log("Keyword Data:", data);
-}
-document.addEventListener("DOMContentLoaded", loadKeywordData);
